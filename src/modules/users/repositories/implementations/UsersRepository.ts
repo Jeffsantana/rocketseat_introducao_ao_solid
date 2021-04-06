4ee119fca67483bf8b199dbdd76fff8431e48408
SantanaJeff <jeffersonsantana2008@hotmail.com>
2021-04-05 22:18:46 -0300

    let's go

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,59 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+    Object.assign(user, {
+      name,
+      email,
+      admin: false,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new Error("Mensagem do erro");
+    }
+    return user;
+  }
+  verifyAdmin(id: string): User | undefined {
+    const user = this.users.find((user) => {
+      if (user.id === id && user.admin) return user;
+    });
+    if (!user) {
+      throw new Error("Mensagem do erro");
+    }
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+    if (!user) {
+      throw new Error("Mensagem do erro");
+    }
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const { id } = receivedUser;
+    const user = this.users.find((user) => {
+      if (user.id === id) {
+        user.admin = true;
+        user.updated_at = new Date();
+        return user;
+      }
+    });
+    return user;
   }
 
   list(): User[] {
-    // Complete aqui
+    const { users } = this;
+    return users;
   }
 }
 
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -7,10 +7,19 @@ interface IRequest {
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
+  execute({ email, name }: IRequest): void {
+    const userAlreadyExist = this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExist) {
+      throw new Error("User Already Exists");
+    } else {
+      this.usersRepository.create({
+        name,
+        email,
+      });
+    }
   }
 }
 
